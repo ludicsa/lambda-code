59d75e7162f1a63601a81bf0bdaf067e9122d711
ludicsa <lucas.ludicsa99@gmail.com>
2025-01-30 20:32:49 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,5 +3,9 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
         "body": json.dumps({"message": "Hello, World!"})
     }
